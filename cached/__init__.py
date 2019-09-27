b859b63edc415b9466740c71fcae2fded8a0bb2f
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2019-09-27 10:24:28 -0700

    Add TODO note.

--- cached/__init__.py
+++ cached/__init__.py
@@ -5,4 +5,6 @@ __author__ = 'Kenneth VanderLinde'
 __email__ = 'kwvanderlinde@gmail.com'
 __version__ = '0.0.0'
 
+# TODO Get proper logging going instead of print() statements.
+
 from .adapter import CachedHTTPAdapter, create
